f007d32fe04e59daed7eb079072ef8f371454a13
Neerazbhat <neeraz.bhat@gmail.com>
2024-02-14 12:44:34 +0545

    feat: variables in detail

---
+++ 01-variables-datatypes/02-variables/script.js
@@ -0,0 +1,30 @@
+// Keywords to create a variables
+// let, var and const
+
+// let firstName = "Niraj";
+// let lastName;
+
+// console.log(firstName, lastName);
+
+// lastName = "bhat";
+
+// var address = "Motichowk";
+// var email;
+
+// console.log(address, email);
+
+// email = "niraj@test.com";
+
+// const tax = 13;
+
+// console.log(tax);
+
+const person = {
+  name: "Niraj Bhat",
+  address: "Nepal",
+};
+
+person.name = "New changed";
+person.email = "niraj@test.com";
+
+console.log(person);
